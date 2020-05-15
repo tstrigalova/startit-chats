c73ab774fe20d6f9f08cf437326eae326af2df15
tstrigalova <53894541+tstrigalova@users.noreply.github.com>
2020-05-15 17:50:51 +0300

    LaiksNojauna

--- chats.py
+++ chats.py
@@ -1,21 +1,21 @@
 from flask import json, jsonify
 from datetime import datetime
-
+timestamp = 1545730073
+laiks = datetime.fromtimestamp(timestamp)
 
 LOGFAILS = "chats.txt"
 
 
 def lasi():
     chata_rindas = []
-    timestamp = 1545730073
-    laiks = datetime.fromtimestamp(timestamp)
+
     with open(LOGFAILS, "r", encoding="utf-8") as f:
         for rinda in f:
             chata_rindas.append(json.loads(rinda))
-    return jsonify({"chats": chata_rindas, laiks})
+    return jsonify({"chats": chata_rindas})
 
 
 def pieraksti_zinju(dati):
     with open(LOGFAILS, "a", newline="", encoding="utf-8") as f:
-        f.write(json.dumps(dati["chats"]) + "\n")
+        f.write(json.dumps(dati["chats"]) + "\n" + "laiks")
 
